42cc3e7b4636ad9362f8ce831317049b6f74747c
juyeon6069 <97582404+juyeon6069@users.noreply.github.com>
2023-11-07 17:50:18 -0500

    Update index.js

--- project/server/index.js
+++ project/server/index.js
@@ -9,7 +9,7 @@ const app = express()
 // SETUP THE MIDDLEWARE
 app.use(express.urlencoded({ extended: true }))
 app.use(cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://0.0.0.0:3000"],
     credentials: true
 }))
 app.use(express.json())
